5f4afc5b390cfa7134fd3e72747a1ef583b001d6
Jack Shelton <me@jackshelton.com>
2023-02-26 17:33:43 -0600

    mobile faq + some desktop changes

---
+++ src/components/FAQ.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import * as Accordion from '@radix-ui/react-accordion';
+import styles from './FAQ.module.css';
+import data from '../constants';
+import downArrow from '../../public/icon-arrow-down.svg';
+
+function FAQ() {
+  return (
+    <Accordion.Root type="single" collapsible={true}>
+      {data.map(({ id, question, answer }) => (
+        <Accordion.Item className={styles.item} key={id} value={id}>
+          <Accordion.Header>
+            <Accordion.Trigger className={styles.trigger}>
+              {question}
+              <img className={styles.svg} src={downArrow} alt="" />
+            </Accordion.Trigger>
+          </Accordion.Header>
+          <Accordion.Content className={styles.content}>
+            {answer}
+          </Accordion.Content>
+        </Accordion.Item>
+      ))}
+    </Accordion.Root>
+  );
+}
+
+export default FAQ;
---
+++ src/constants.js
@@ -0,0 +1,34 @@
+const data = [
+  {
+    id: 'team-members',
+    question: 'How many team members can I invite?',
+    answer:
+      'You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.',
+  },
+  {
+    id: 'max-file-size',
+    question: 'What is the maximum file upload size?',
+    answer:
+      'No more than 2GB. All files in your account must fit your allotted storage space.',
+  },
+  {
+    id: 'reset-password',
+    question: 'How do I reset my password?',
+    answer:
+      'Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.',
+  },
+  {
+    id: 'cancel-subscription',
+    question: 'Can I cancel my subscription?',
+    answer:
+      'Yes! Send us a message and we’ll process your request no questions asked.',
+  },
+  {
+    id: 'additional-support',
+    question: 'Do you provide additional support?',
+    answer:
+      'Chat and email support is available 24/7. Phone lines are open during normal business hours.',
+  },
+];
+
+export default data;
